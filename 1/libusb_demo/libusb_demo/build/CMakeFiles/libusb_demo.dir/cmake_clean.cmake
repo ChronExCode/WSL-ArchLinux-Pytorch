57a54924174ade85c650925ea6dc22b3037c0199
ChronExCode <chronexcode@gmail.com>
2026-04-14 00:23:08 -0700

    Add files via upload

---
+++ 1/libusb_demo/libusb_demo/build/CMakeFiles/libusb_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libusb_demo.dir/link.d"
+  "CMakeFiles/libusb_demo.dir/main.cpp.o"
+  "CMakeFiles/libusb_demo.dir/main.cpp.o.d"
+  "libusb_demo"
+  "libusb_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libusb_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1/libusb_demo/libusb_demo/build/CMakeFiles/libusb_demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libusb_demo.
+# This may be replaced when dependencies are built.
